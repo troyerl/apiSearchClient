7ca2274a015e36e796d69a8e39bd07f51b4f1555
Logan Troyer <troyerlogan@gmail.com>
2020-07-20 15:54:18 -0400

    Fixed title screen

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Matts a loser. Thats all this website is for.
         </p>
         <a
           className="App-link"
